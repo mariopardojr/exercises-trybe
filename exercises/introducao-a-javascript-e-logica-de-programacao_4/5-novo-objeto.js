3320000e23f59a289534c6c663fe797377e69898
Mario Pardo <mariio.pardojr@gmail.com>
2020-10-29 10:19:22 -0400

    Novo objeto

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/5-novo-objeto.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: "Margarida e Tio Patinhas",
+    origem: "Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas",
+    recorrente: "Ambos recorrentes"
+  };
+  for(key in info){
+      console.log(info[key]);
+  }
