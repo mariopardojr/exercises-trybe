0e7bbdd85a1f0de2c2d940d0d4bda9bbfa9ecbd6
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-05 11:29:20 -0400

    Correcao exercicio 1

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -18,10 +18,24 @@ window.onload = function () {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     let diasLista = document.getElementById("days");
     for (let index = 0; index < dezDaysList.length; index += 1) {
-        let dia = document.createElement("li");
-        dia.className  = "day";
-        dia.innerText = dezDaysList[index];
-        diasLista.appendChild(dia);
+        let diaMes = dezDaysList[index];
+        let day = document.createElement("li");
+        if (diaMes === 24 || diaMes === 31) {
+            day.className = "day holiday";
+            diasLista.appendChild(day);
+        } else if (diaMes === 4 || diaMes === 11 || diaMes === 18) {
+            day.className = "day friday";
+            diasLista.appendChild(day);
+        } else if (diaMes === 25) {
+            day.className = "day friday holiday";
+            diasLista.appendChild(day);
+        } else {
+            day.className = "day"
+            diasLista.appendChild(day);
+        }
+        day.innerText = diaMes;
+        console.log(day);
+        
     }
     
 
