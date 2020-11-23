170eb7ff6e49ab1c26afb210497d028709866351
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-23 17:55:01 -0400

    Exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script-p1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script-p1.js
@@ -8,5 +8,21 @@ const testingScope = (escopo) => {
     console.log(elseScope);
   }
 }
-
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// const ordenaArray = () => {
+//   oddsAndEvens[0] = 2;
+//   oddsAndEvens[1] = 3;
+//   oddsAndEvens[2] = 4;
+//   oddsAndEvens[3] = 7;
+//   oddsAndEvens[4] = 10;
+//   oddsAndEvens[5] = 13;
+//   return oddsAndEvens
+// }
+
+// console.log(ordenaArray());
+
+const ordenaArray = () => oddsAndEvens.sort((a, b) => a - b);
+console.log(ordenaArray());
