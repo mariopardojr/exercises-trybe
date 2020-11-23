6e2851866dba72f67aad8aa1d590c43ba6025c38
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-23 18:38:24 -0400

    Exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script-p2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script-p2.js
@@ -7,3 +7,19 @@ const fatorial = (num) => {
 };
 console.log(fatorial(4));
 
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+const maiorPalavra = frase => {
+  let fraseArray = frase.split(' ');
+  let maior = 0
+  let resultado = '';
+  for (palavra of fraseArray) {
+    if (palavra.lenght > maior) {
+      maior = palavra.lenght
+      resultado = palavra;
+    }
+  }
+  return palavra;
+}
+console.log(maiorPalavra("Antônio foi no banheiro e não sabemos o que aconteceu"))
+
