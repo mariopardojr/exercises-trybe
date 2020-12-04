e53a950cadd9006c48bf5f2ebd3e11855b20bb8a
Mario Pardo <mariio.pardojr@gmail.com>
2020-12-04 10:31:57 -0400

    Commit

---
+++ exercises/higher-order-functions-do-javascript-es6_3/exercise1.js
@@ -0,0 +1,16 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  const getTogether = (accumulator, array) => accumulator.concat(array);
+  return arrays.reduce(getTogether, []);
+}
+console.log(flatten());
+// assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
---
+++ exercises/higher-order-functions-do-javascript-es6_3/exercise5.js
@@ -0,0 +1,19 @@
+
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  const nameCountA = names.map((name) => {
+    const nameSplit = name.split('');
+    
+  })
+}
+
+assert.deepStrictEqual(containsA(), 20);
